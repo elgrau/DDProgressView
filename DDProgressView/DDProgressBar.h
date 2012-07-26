48f1ec204e590a0d6ab01b7b30c528bdfa6e6787
Miguel Ángel Grau <grau.informatica@gmail.com>
2012-07-27 01:29:38 +0200

    The progress bar allows multiple values and colors

---
+++ DDProgressView/DDProgressBar.h
@@ -0,0 +1,13 @@
+//
+//  DDProgressBar.h
+//  DDProgressView
+//
+//  Created by Miguel Ángel Grau Martínez on 26/07/12.
+//  Copyright (c) 2012 Acrossair. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDProgressBar : NSObject
+
+@end
---
+++ DDProgressView/DDProgressBar.m
@@ -0,0 +1,13 @@
+//
+//  DDProgressBar.m
+//  DDProgressView
+//
+//  Created by Miguel Ángel Grau Martínez on 26/07/12.
+//  Copyright (c) 2012 Acrossair. All rights reserved.
+//
+
+#import "DDProgressBar.h"
+
+@implementation DDProgressBar
+
+@end
